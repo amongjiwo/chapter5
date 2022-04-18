f574d2679b16a38d71995c1bc8a2997cf6cf251d
amongjiwo <dahlanakhmad@gmail.com>
2022-04-18 16:40:33 +0700

    create landing.ejs and landing.css

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const port = 8080;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
 app.listen(port, () => {
     console.log(`Listening on ${port}`);
 });
